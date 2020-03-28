4268752b19b988455484fb48a27575ee71bf3bcf
Shaps Benkau <shapsuk@me.com>
2020-03-28 15:47:05 +0000

    Adds skeleton VC

--- Sources/ComposedMediaUI/MediaViewContoroller.swift
+++ Sources/ComposedMediaUI/MediaViewContoroller.swift
@@ -1,15 +1,55 @@
 import UIKit
 import Composed
 import ComposedUI
+import ComposedLayouts
 
 open class MediaViewController: UIViewController {
 
-    @IBOutlet lazy open var collectionView: UICollectionView = {
+    open private(set) lazy var collectionView: UICollectionView = {
+        return UICollectionView(frame: .zero, collectionViewLayout: layout)
+    }()
+
+    open private(set) lazy var collectionCoordinator: CollectionCoordinator = {
+        #warning("Replace with appropriate provider when available")
+        let provider = ComposedSectionProvider()
+        return CollectionCoordinator(collectionView: collectionView, sectionProvider: provider)
+    }()
+
+    private lazy var layout: UICollectionViewLayout = {
         let layout: UICollectionViewLayout
 
-        layout = UICollectionViewCompositionalLayout(section: NSCollectionLayoutSection(group: NSCollectionLayoutGroup(layoutSize: NSCollectionLayoutSize(widthDimension: .fractionalWidth(1), heightDimension: .fractionalHeight(1))))
-
-        let view = UICollectionView(frame: .zero, collectionViewLayout: layout))
+        if #available(iOS 13.0, *) {
+            return UICollectionViewCompositionalLayout { [weak self] index, environment in
+                guard let self = self,
+                    self.collectionCoordinator.sectionProvider.sections.indices.contains(index),
+                    let section = self.collectionCoordinator.sectionProvider.sections[index] as? CompositionalLayoutHandler else { return nil }
+                return section.compositionalLayoutSection(environment: environment)
+            }
+        } else {
+            return UICollectionViewFlowLayout()
+        }
     }()
+
+    open override func viewDidLoad() {
+        super.viewDidLoad()
+
+        if #available(iOS 13.0, *) {
+            collectionView.backgroundColor = .systemBackground
+        } else {
+            collectionView.backgroundColor = .groupTableViewBackground
+        }
+
+        collectionView.alwaysBounceVertical = true
+        collectionView.setCollectionViewLayout(layout, animated: false)
+        collectionView.frame = view.bounds
+        collectionView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        view.addSubview(collectionView)
+    }
+
+    /// Required specifically for `UICollectionViewFlowLayout` since some rotations don't cause an invalidation!?
+    open override func viewWillTransition(to size: CGSize, with coordinator: UIViewControllerTransitionCoordinator) {
+        super.viewWillTransition(to: size, with: coordinator)
+        self.collectionCoordinator.invalidateLayout()
+    }
 
 }
