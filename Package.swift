e877a2ba521ffc12ddb93a2f3ba1b64e5451b4f5
Shaps Benkau <shapsuk@me.com>
2020-05-04 09:22:09 +0100

    Updates to ComposedUI 1.0.0

--- Package.swift
+++ Package.swift
@@ -15,8 +15,8 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/composed-swift/ComposedMedia", from: "0.0.0"),
-        .package(url: "https://github.com/composed-swift/ComposedUI", from: "0.0.0"),
-        .package(url: "https://github.com/composed-swift/ComposedLayouts", from: "0.0.0"),
+        .package(url: "https://github.com/composed-swift/ComposedUI", from: "1.0.0"),
+        .package(url: "https://github.com/composed-swift/ComposedLayouts", from: "1.0.0"),
     ],
     targets: [
         .target(
